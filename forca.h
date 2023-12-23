47adec915aa652a6b084ab579161f83a50e07fd7
Débora <deb.limad@gmail.com>
2023-12-23 19:10:04 -0300

    Jogo da Forca

---
+++ forca.h
@@ -0,0 +1,19 @@
+void abertura();
+void escolhePalavra();
+void chuta();
+int jaChutou(char letra);
+void desenhaForca();
+int letraExiste(char letra);
+int chutesErrados();
+int enforcou();
+int ganhou();
+void adicionarPalavra();
+
+void escolhaDificuldade();
+int defineDificuldade();
+
+int verificarPalavrasIguais(char* palavra1, char* palavra2);
+int palavraJaExiste(FILE* f, char* novaPalavra, int qtdPalavras);
+
+int calcPontuacaoPlayer();
+void insertPlayerRanking();
